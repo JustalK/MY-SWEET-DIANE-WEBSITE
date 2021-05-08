d2d901cac7007044ba66b4fe30203fabda898b77
Latsuj <justal.kevin@gmail.com>
2021-05-09 00:41:48 +0800

    [DOCUMENTATION] Adding comment for explaining the import of not used module

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@src/styles/globals.scss'
 import styles from '@src/styles/App.module.scss'
+// I added those components because without the build is not working properly
+// The module css are remove to early for doing page transitions
+// For more information : https://github.com/vercel/next.js/issues/17464
 import Main from './index.js'
 import Histories from './histories'
 import Moments from './moments'
